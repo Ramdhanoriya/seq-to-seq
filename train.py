a3b1eec7e1d1325f6342f99feaa8e29346b26358
Pranoy Radhakrishnan <pranoyalkr@gmail.com>
2018-05-14 22:38:17 +0530

    Update train.py

--- train.py
+++ train.py
@@ -12,8 +12,6 @@ from nltk import word_tokenize
 import pickle
 from keras.preprocessing import sequence
 
-
-
 # one hot encode
 def one_hot_encode(y, max_int):
     yenc = list()
@@ -54,21 +52,18 @@ def define_models(n_input, n_output, n_units):
     return model, encoder_model, decoder_model
 
 
-
 dataX=[]
 dataY=[]
-
 X1=[]
 X2=[]
 Y=[]
 
-
+# number of timesteps for encoder and decoder
 n_in = 10
 n_out = 20
 
-
+# load dataset
 data=open("dataset.txt","r").read().lower()
-
 for i in data.split("\n\n"):
     a=i.split("\n")
     question=a[0]
@@ -76,93 +71,63 @@ for i in data.split("\n\n"):
     dataX.append(question)
     dataY.append(answer)
 
-
+# creating dict for integer encoding
 words=open("words.txt").read().split()
 word_to_int_input = dict((c, i+2) for i, c in enumerate(words))
 int_to_word_input = dict((i+2, c) for i, c in enumerate(words))
-
-
 word_to_int_input.update({"padd":0})
 int_to_word_input.update({0:"padd"})
-
-
 word_to_int_input.update({"_":1})
 int_to_word_input.update({1:"_"})
 
 encoded_length = len(word_to_int_input)
-
+# integer encoding
 for sentence in dataX:
-
     sentence=word_tokenize(sentence)
     sentence = [word for word in sentence if word.isalpha()]
     X1.append([word_to_int_input[word] for word in sentence])
-
-
 for sentence in dataY:
-
     sentence=word_tokenize(sentence)
     sentence = [word for word in sentence if word.isalpha()]
     Y.append([word_to_int_input[word] for word in sentence])
-
-
-
 for sentence in dataY:
-
     sentence=word_tokenize(sentence)
     sentence = [word for word in sentence if word.isalpha()]
     sentence.insert(0,"_")
     sentence=sentence[0:len(sentence)-1]
     X2.append([word_to_int_input[word] for word in sentence])
 
-
-
-
+# zero padding
 X1 = sequence.pad_sequences(X1, maxlen=n_in,padding='post')
 X2 = sequence.pad_sequences(X2, maxlen=n_out,padding='post')
 Y = sequence.pad_sequences(Y, maxlen=n_out,padding='post')
 
-
-
+# one hot encode
 X1=one_hot_encode(X1,encoded_length)
 X2=one_hot_encode(X2,encoded_length)
 Y=one_hot_encode(Y,encoded_length)
-
-
 X1=array(X1)
 X2=array(X2)
 Y=array(Y)
-
-# configure problem
 
 # define model
 train, infenc, infdec = define_models(encoded_length, encoded_length, 128)
 train.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
 print(train.summary())
 
-
-
-print(X1.shape,X2.shape,Y.shape)
 # train model
 train.fit([X1, X2], Y, epochs=1500)
 
-
-
-
-
+# saving model
 infenc.save_weights("model_enc.h5")
 print("Saved model to disk")
-
-
-
 infdec.save_weights("model_dec.h5")
 print("Saved model to disk")
 
-
-
+# saving integer encodings dict
 save_word_features = open("word_to_int_input.pickle","wb")
 pickle.dump(word_to_int_input, save_word_features)
 save_word_features.close()
-
 save_word_features = open("int_to_word_input.pickle","wb")
 pickle.dump(int_to_word_input, save_word_features)
 save_word_features.close()
